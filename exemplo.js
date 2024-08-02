19aed7a67508297dd9e836026869f18632288354
Gustavo <162714951+gustavodaque@users.noreply.github.com>
2024-08-02 11:31:29 -0300

    Add files via upload

---
+++ exemplo.js
@@ -0,0 +1,40 @@
+//Manipulação de Strings
+let primeiroNome = "Gustavo"
+let ultimoNome = "Ferrari"
+let nomeCompleto = primeiroNome + " " + ultimoNome
+console.log(nomeCompleto)
+
+
+let numero1 = 10
+let numero2 = 20
+
+let soma = numero1 + numero2
+let subtracao = numero1 - numero2
+let multiplicacao = numero1 * numero2
+let divisao = numero1 / numero2
+console.log(soma, subtracao, multiplicacao, divisao)
+
+
+let estaChovendo = falseif(estaChovendo) {
+    console.log("Está Chovendo")
+}else{
+    console.log("Não está chovendo")
+}
+
+let resposta = nu11
+let nota 
+console.log(resposta, nota)
+
+
+let iduUnico = Symbol()
+let produto ={
+    (idUnico): 123;
+    nome: "Camiseta"
+}
+console.log(produto(idUnico), produto.nome)
+
+
+let numeroGrande = BigInt("")
+let numero =
+let numero3 = numeroGrande
+console.log(numeroGrande, numero, "\n", numero3)
